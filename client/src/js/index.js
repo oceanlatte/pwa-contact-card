6cecb604a97fdbaeb98b674a04890e46eb8b57ae
ocean <osciris.veliz@gmail.com>
2022-09-22 17:56:02 -0500

    import compiled CSS module; bootstrap connected

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -2,16 +2,19 @@
 import "./form";
 import "./submit";
 
+// Import CSS files
+import "../css/index.css";
+import { Tooltip, Toast, Popover } from 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 // Import Images
 import Logo from '../images/logo.png';
 import Bear from '../images/bear.png';
 import Dog from '../images/dog.png';
 
+// Add images on load
 window.addEventListener('load', function () {
   document.getElementById('logo').src = Logo;
   document.getElementById('bearThumbnail').src = Bear;
   document.getElementById('dogThumbnail').src = Dog;
 });
-
-// Import CSS files
-import "../css/index.css";
